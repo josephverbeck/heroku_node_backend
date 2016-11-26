d3a7d5e625732fae5fc145895425abbb1c7b57c0
josephverbeck <joseph.verbeck@nih.gov>
2016-11-26 18:52:19 -0500

    Start response

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/', function(request, response) {
+  response.json({"hello": "Node"});
+});
+
 app.listen(app.get('port'), function(){
 	console.log('Node is running on port', app.get('port'));
 });
