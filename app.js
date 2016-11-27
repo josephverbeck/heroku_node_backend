0157dc3fcbe9c4be77b248074a83895e45f2af7d
josephverbeck <joseph.verbeck@nih.gov>
2016-11-27 14:58:38 -0500

    Adding postgres

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var app = express();
+var pg = require('pg');
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
 
 app.get('/', function(request, response) {
   response.json({"hello": "Node"});
