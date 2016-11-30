5548a2eb6e238b65916301a3eeebbe7445ac82cf
josephverbeck <joseph.verbeck@nih.gov>
2016-11-30 16:31:47 -0500

    Fixing semi issue in all files

--- public/routes.js
+++
@@ -1,8 +0,0 @@
-bodyParser = require(‘body-parser’);
-
-module.exports = function(app) {
- app.use(bodyParser.json());
- app.get('/', function(request, response) {
-  response.json({"hello": "Node"});
- });
-};
